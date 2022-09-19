963bb3bbaff54951f54b0031d66ab6863b3c91d9
cinusko <77555201+cinusko@users.noreply.github.com>
2022-09-19 18:14:59 +0200

    Update Program.cs

--- Open-Lab-01.00/Program.cs
+++ Open-Lab-01.00/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_01._00
 {
@@ -6,7 +6,7 @@ namespace Open_Lab_01._00
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Apps Lab!");
         }
     }
 }
